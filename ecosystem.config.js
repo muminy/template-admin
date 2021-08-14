d40e72a086ca730cf6d0b353eb51d0f669b5ed72
Mümin Yıldırım <mmnyldrm61@gmail.com>
2021-08-14 17:54:02 +0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "next-site",
+      cwd: " /home/your-name/my-nextjs-project",
+      script: "npm",
+      args: "start",
+      env: {},
+    },
+    // optionally a second project
+  ],
+};
